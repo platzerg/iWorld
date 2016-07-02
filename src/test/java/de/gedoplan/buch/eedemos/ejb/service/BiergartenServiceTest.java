905be5f8c148491294a25951a18cc63ff0eb2f42
platzerg <guenter.platzerworld@googlemail.com>
2016-07-02 14:08:22 +0200

    added Biergarten

--- src/test/java/de/gedoplan/buch/eedemos/ejb/service/BiergartenServiceTest.java
+++ src/test/java/de/gedoplan/buch/eedemos/ejb/service/BiergartenServiceTest.java
@@ -6,6 +6,7 @@ import de.gedoplan.buch.eedemos.ejb.util.LookupHelper;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Properties;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -17,6 +18,7 @@ public class BiergartenServiceTest
 {
   private static Context jndiCtx;
   private static String  lookupName;
+  private static BiergartenService biergartenService;
 
   @BeforeClass
   public static void beforeClass() throws Exception
@@ -24,8 +26,26 @@ public class BiergartenServiceTest
     // Anmeldung am JNDI
     jndiCtx = new InitialContext();
 
+    Properties jndiProps = new Properties();
+    jndiProps.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
+    jndiProps.put(Context.PROVIDER_URL, "http-remoting://localhost:8080");
+    jndiProps.put("jboss.naming.client.ejb.context", true);
+    Context context = new InitialContext(jndiProps);
+    // java:module/iWorld/BiergartenService!com.platzerworld.service.BiergartenService
+    //biergartenService = context.doLookup("java:module/iWorld/BiergartenService!com.platzerworld.service.BiergartenService");
+    //biergartenService = (BiergartenService)context.lookup("java:comp/iWorld/BiergartenService!com.platzerworld.service.BiergartenService");
+
+    InitialContext context1 = new InitialContext();
+
+    //Lookup durchführen:
+    biergartenService = (BiergartenService) context.lookup("ejb:/iWorld//BiergartenService!com.platzerworld.service.BiergartenService");
+
+
+    //ejb:iWorld/BiergartenService!com.platzerworld.service.BiergartenService?stateless
+    //ejb:iWorld/BiergartenService!com.platzerworld.service.BiergartenService
+
     // Lookup-Namen der EJB ermitteln
-    lookupName = LookupHelper.getEjbLookupName(BiergartenService.class, false);
+    //lookupName = LookupHelper.getEjbLookupName(BiergartenService.class, false);
   }
 
 
